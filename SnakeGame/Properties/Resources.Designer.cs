8664feb5936bc8f2d557a4f204221d3328ab93bf
lthnoname <henryle987654@gmail.com>
2025-12-04 12:57:42 +0700

    cập nhật tài nguyên cho game background

--- SnakeGame/Properties/Resources.Designer.cs
+++ SnakeGame/Properties/Resources.Designer.cs
@@ -121,6 +121,16 @@ namespace SnakeGame.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GameBackground2 {
+            get {
+                object obj = ResourceManager.GetObject("GameBackground2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] Icon {
